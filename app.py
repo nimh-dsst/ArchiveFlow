ffbe4aae37ae71963cfc9c48f05e58f96bf16b68
Joshua Lawrimore <josh.lawrimore@gmail.com>
2025-02-27 17:11:51 -0500

    Draft of app.py is ready for user testing

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
-import json
+import time
 from pathlib import Path
 from xml.etree import ElementTree as ET
+from xml.etree.ElementTree import Element
 
-import pandas as pd
 import streamlit as st
 from requests.exceptions import SSLError
 from streamlit import session_state as ss
 
 from archiveflow.api import LAClient
-from archiveflow.behavior_widget import (
-    BehaviorFormV4,
-    BehaviorFormV6,
-    EmptyResults,
-    parse_behavior_widget,
-    recontruct_behavior_form,
-)
 from archiveflow.config import config
+from archiveflow.structure import TejedaExperiment
 
 # Session state variables
 if "logged_in" not in ss:
@@ -32,34 +26,43 @@ if "nbid_radio" not in ss:
     ss.nbid_radio = nbid_radio
 if "nbid" not in ss:
     ss.nbid = None
-if "behavior_forms" not in ss:
-    behavior_forms: list[BehaviorFormV4 | BehaviorFormV6] = []
-    ss.behavior_forms = behavior_forms
+if "experiments" not in ss:
+    experiments: dict[str, ET.Element] = {}
+    ss.experiments = experiments
+if "experiment_radio" not in ss:
+    experiment_radio: str | None = None
+    ss.experiment_radio = experiment_radio
+if "folder_str" not in ss:
+    folder_str: str | None = None
+    ss.folder_str = folder_str
+if "folder_path" not in ss:
+    folder_path: Path | None = None
+    ss.folder_path = folder_path
+if "write_ready" not in ss:
+    write_ready: bool = False
+    ss.write_ready = write_ready
+if "folder_select" not in ss:
+    folder_select: bool = False
+    ss.folder_select = folder_select
+if "method" not in ss:
+    method: str | None = None
+    ss.method = method
 
 
-@st.cache_data
-def get_behavior_forms():
+def get_experiment_nodes() -> None:
     assert ss.client.is_auth
-    pages = ss.client.get_all_pages(nbid=ss.nbid)
-    for page in pages:
-        tree_id = page.find("tree-id")
-        if isinstance(tree_id, ET.Element):
-            tree_id_text = tree_id.text
-            if isinstance(tree_id_text, str):
-                response = ss.client.get_entry_data(
-                    nbid=ss.nbid, page_tree_id=tree_id_text
-                )
-                try:
-                    meta, forms = parse_behavior_widget(response)
-                    reconstructed_form = recontruct_behavior_form(meta, forms)
-                    ss.behavior_forms.append(reconstructed_form)
-                except ValueError:
-                    continue
-                except EmptyResults:
-                    continue
+    experiment_nodes: list[ET.Element] = ss.client.get_dir_nodes(nbid=ss.nbid)
+    experiments: dict[str, Element] = {}
+    for experiment_node in experiment_nodes:
+        experiment_name = experiment_node.findtext("display-text")
+        experiment_id = experiment_node.findtext("tree-id")
+        if isinstance(experiment_name, str) and isinstance(experiment_id, str):
+            experiments[experiment_name] = experiment_node
+    ss.experiments = experiments
 
 
-st.title("Archive Flow")
+st.title("Tejeda Lab")
+st.header("Experiment Directory Creator")
 if not ss.logged_in:
     ss.button_auth = st.button("Login to Lab Archives")
 if ss.button_auth:
@@ -115,43 +118,85 @@ if ss.logged_in:
 if ss.nbid_radio and isinstance(ss.notebook_map, dict):
     ss.nbid = ss.notebook_map[ss.nbid_radio]
     st.button(
-        "Search selected notebook for Behavior Entries",
-        on_click=get_behavior_forms,
+        "Get Project ",
+        on_click=get_experiment_nodes,
     )
-if ss.behavior_forms:
-    st.write(f"Found {len(ss.behavior_forms)}")
-    df = pd.DataFrame([form.metadata for form in ss.behavior_forms])
-    st.dataframe(data=df)
-    if len(ss.behavior_forms) > 0:
-        root_dir = st.text_input("Root Directory")
-        root_path = Path(root_dir)
-        if not root_path.exists():
-            st.warning(
-                "Root directory does not exist. Please choose an existing directory!"
-            )
-        else:
-            do_write = st.button("Write Behavior Data")
-            if do_write:
-                # TODO: parse the existing folder provided by the user
-                # compare with the existing directory structure in the notebook
-                # warn user of any discrepancies
-                # if discrepancies, ask user if they want to proceed
-                # populate the local with updates from the API
-                for form in ss.behavior_forms:
-                    form_path = root_path.joinpath(
-                        form.metadata["Date"].replace("/", "_")
+
+if len(ss.experiments) > 0:
+    ss.experiment_radio = st.selectbox(
+        "Experiments",
+        ss.experiments.keys(),
+        on_change=get_experiment_nodes,
+    )
+
+if ss.experiment_radio:
+    ss.method = st.radio("Make Method", ["Existing", "All"])
+    if ss.method == "All":
+        st.text(
+            "All experiment folders well be created."
+            + "Intended for new experiment setup."
+        )
+    elif ss.method == "Existing":
+        st.text("Folders existing in LabArchives will be created.")
+    if st.button("Select Folder"):
+        ss.folder_select = True
+    if ss.folder_select:
+        ss.folder_str = st.text_input("Enter folder path:", key="folder_input")
+        if ss.folder_str:
+            ss.folder_path = Path(ss.folder_str)
+            if not ss.folder_path.exists():
+                st.warning(
+                    "Folder does not exist! "
+                    + "Please make the folder or enter a different path."
+                )
+                if st.button("Make Folder?"):
+                    ss.folder_path.mkdir(parents=True)
+                    ss.write_ready = True
+                    st.rerun()
+            else:
+                st.text("Folder ready for writing!")
+                ss.write_ready = True
+        if ss.write_ready:
+            if st.button("Write Directory Structure to Local"):
+                if (
+                    ss.folder_path
+                    and ss.client
+                    and ss.nbid
+                    and ss.experiments[ss.experiment_radio]
+                    and ss.method
+                ):
+                    exp: TejedaExperiment = TejedaExperiment(
+                        experiment_root_dir=ss.folder_path,
+                        client=ss.client,
+                        nbid=ss.nbid,
+                        experiment=ss.experiments[ss.experiment_radio],
+                        make_method=ss.method,  # type: ignore
                     )
-                    form_path.mkdir()
-                    with open(
-                        form_path.joinpath("metadata.json"), "w"
-                    ) as f_out:
-                        json.dump(form.metadata, f_out, indent=4)
-                    with open(form_path.joinpath("notes.txt"), "w") as f_out:
-                        f_out.write(form.notes)
-                    form.first_table.to_excel(
-                        form_path.joinpath("first_table.xlsx")
-                    )
-                    form.second_table.to_excel(
-                        form_path.joinpath("second_table.xlsx")
-                    )
-                do_write = False
+                    exp.create_experiment_dirs()
+                    if exp.behavior is not None:
+                        exp.behavior.create_cohorts()
+                    if exp.histology is not None:
+                        exp.histology.create_cohorts()
+                    if exp.surgeries is not None:
+                        exp.surgeries.create_cohorts()
+                    if exp.metadata is not None:
+                        exp.metadata.create_cohorts()
+                    if exp.photometry is not None:
+                        exp.photometry.create_cohorts()
+                    st.success("Experiment directories created!")
+if st.button("Reset Experiment Selection"):
+    # Clear the text input by changing its key
+    st.text_input(
+        "Enter folder path:",
+        key="folder_input_" + str(time.time()),
+    )
+    ss.experiment_radio = None
+    ss.experiments = {}
+    ss.method = None
+    ss.folder_select = False
+    ss.write_ready = False
+    ss.folder_path = None
+    ss.folder_str = None
+    ss.nbid_radio = None
+    ss.nbid = None
+    st.rerun()
